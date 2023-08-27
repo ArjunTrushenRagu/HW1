a222c9fa62f5d1cc7e750f54ac1345703d7fc45b
Arjun Ragu <arjun_ragu@outlook.com>
2023-08-26 22:18:59 -0500

    reworking homework1 string manipulation and filtering

---
+++ Homework1/Homework1/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Homework1
+//
+//  Created by Arjun Ragu on 8/26/23.
+//
+
+#include <iostream>
+
+// Includes
+
+// Data structures for storing info
+
+// Core functions for decoding
+
+// Utility functions for parsing and other operations
+
+// File handling functions
+
+// Main function:
+// 1. Read the file
+// 2. Process the data
+// 3. Write results to the output
